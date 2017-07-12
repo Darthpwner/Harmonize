92bae7941c2b369cec702b51e94203e962dcbe19
Darthpwner <matthewallenlin@gmail.com>
2017-07-12 16:01:52 -0700

    Right aligned score at the top

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
+        <Text>Score: 0</Text>
+
+
       </View>
     );
   }
@@ -17,7 +21,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    alignItems: 'flex-end'
+
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
